7251861654094c3dd99386f8c417ea144f9d8eff
guozhiyuan <237624988@qq.com>
2024-07-30 17:00:39 +0800

    增加小关成功和失败的音效

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -186,11 +186,22 @@ Page({
         }
       });
 
+      const innerAudioContext = wx.createInnerAudioContext()
+      innerAudioContext.autoplay = true
+      innerAudioContext.onPlay(() => {
+        console.log('开始播放')
+      })
+      innerAudioContext.onError((res) => {
+        console.log(res.errMsg)
+        console.log(res.errCode)
+      })
       if (allCorrect) {
+        innerAudioContext.src = '/pages/music/small_station_success.mp3'
         setTimeout(() => {
           this.nextPage();
         }, 500)
       } else {
+        innerAudioContext.src = '/pages/music/small_station_fail.mp3'
         setTimeout(() => {
           this.retryPage();
         }, 2000)
